543a4b67d9850abf9377d7fd9b52e1950a1b3f6d
James Stuckey <jstuckey@rutgers.edu>
2018-05-31 17:59:01 -0400

    added update folder endpoint

--- routes/folders.js
+++ routes/folders.js
@@ -18,8 +18,54 @@ router.get('/', (req, res, next) => {
         });
 });
 
-// router.get('folders/:id', (req, res, next) => {
-//     knex.select()
-// });
+
+// get single folder
+router.get('/:id', (req, res, next) => {
+    const folder_id = req.params.id;
+
+    knex.select('id', 'name')
+        .from('folders')
+        .where('id', folder_id)
+        .then(result => {
+            res.json(result[0]);
+        })
+        .catch(err => {
+            next(err);
+        });
+
+});
+
+router.put('/:id', (req, res, next) => {
+    const folder_id = req.params.id;
+
+    const updateObj = {};
+    const updatableFields = ['name'];
+
+    updatableFields.forEach(field => {
+        if (field in req.body) {
+            updateObj[field] = req.body[field];
+        }
+    });
+
+    if (!updateObj.name) {
+        const err = new Error('Missing `name` in request body');
+        err.status = 400;
+        return next(err);
+    }
+
+    knex.select('id', 'name')
+        .from('folders')
+        .where('id', folder_id)
+        .update(updateObj)
+        .returning(['name'])
+        .then(result => {
+            res.json(result[0]);
+        })
+        .catch(err => {
+            next(err);
+        });
+});
+
+
 
 module.exports = router;
